c7cd9b9001721e62ff5261c3c315fee4fd8da967
Jaykrish0701 <108427965+Jaykrish0701@users.noreply.github.com>
2022-08-13 18:14:27 +0530

    Add files via upload

---
+++ assignments/SortingUsingArray.java
@@ -0,0 +1,21 @@
+package assignments;
+
+import java.util.Arrays;
+
+public class SortingUsingArray{
+	public static void main(String[] args) {
+	
+		String [] arr = { "HCL","Wipro","Aspire Systems","CTS"};
+		
+		int length = arr.length;
+
+		Arrays.sort(arr);
+			
+		for(int i =length-1;i>=0;i--) 
+		{
+			System.out.print(arr[i] +".");
+		}
+		
+	}
+
+}
